026c19d843d4f79bbf7baa8023785eb958383e92
Bojay B <41974762+BojayB@users.noreply.github.com>
2026-07-06 14:17:16 -0400

    Add files via upload
    restart

---
+++ commands/sum buuullshit/badge.js
@@ -0,0 +1,9 @@
+﻿const {SlashCommandBuilder} = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('badge')
+    .setDescription('Gives Bojay B his Active Bot Discord Badge or whatever.'),
+    async execute(interaction) {
+        await interaction.reply('Haha they got rid of this a while ago now lol');
+    },
+};
---
+++ events/interactionCreate.js
@@ -0,0 +1,32 @@
+﻿const { Events, MessageFlags } = require('discord.js');
+
+module.exports = {
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`Command ${interaction.commandName} not found?`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: 'oops error lol!!!',
+                    flags: MessageFlags.Ephemeral,
+                });
+            } else {
+                await interaction.reply({
+                    content: 'oops error lol!!! uhh ping me or something idk',
+                    flags: MessageFlags.Ephemeral,
+                });
+            }
+        }
+    },
+};
